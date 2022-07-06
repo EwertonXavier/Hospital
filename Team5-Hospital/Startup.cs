98ae8a1b6657f4a1f4266814dac1bd4214b28422
Neddlf43 <ewerton.freitas@live.com>
2022-07-06 10:50:56 -0400

    Initial Commit

---
+++ Team5-Hospital/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team5_Hospital.Startup))]
+namespace Team5_Hospital
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
